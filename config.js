aac6747f1dcc8971067da5c5403cada98cb52ed4
commitginza <seto+websocket@commit-watch.co.jp>
2026-07-11 12:14:52 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.HOLOGRAM_CONFIG = {
   // API Gateway HTTP API の /talk URL に差し替えてください。
-  API_URL: 'https://gk1qutuk54.execute-api.ap-northeast-1.amazonaws.com/talk',
+  API_URL: 'https://gk1qutuk54.execute-api.ap-northeast-1.amazonaws.com/session',
 
   // S3上のGLB。ファイル名を変えた場合はここを変更してください。
   MODEL_URL: 'https://watchimg.s3.ap-northeast-1.amazonaws.com/glb/avatar-v2.glb'
